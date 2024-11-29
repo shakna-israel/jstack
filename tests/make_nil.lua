97f8623dc2c8249c8b2c82c67a67c309694769b8
James Milne <s4b3r6@gmail.com>
2024-11-29 19:28:23 +1100

    Testing make_nil

--- tests/make_nil.lua
+++ tests/make_nil.lua
@@ -3,6 +3,10 @@ assert(jstack)
 
 assert(jstack.make_nil)
 
--- jstack.make_nil(caller)
-
--- TODO
+do
+	local s = jstack.make_nil(nil)
+	assert(s)
+	assert(s.content)
+	assert(s.content.type == "symbol")
+	assert(s.content.value == "nil")
+end
