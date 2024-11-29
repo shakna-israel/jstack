1db4adc87ac376d6646aae409ffb2cfe0cfac86d
James Milne <s4b3r6@gmail.com>
2024-11-29 19:50:32 +1100

    Basic tests for stdlib.

--- tests/stdlib.lua
+++ tests/stdlib.lua
@@ -4,3 +4,185 @@ assert(jstack)
 assert(jstack.stdlib)
 
 -- TODO
+
+assert(type(jstack.stdlib()) == "table")
+
+do
+	local clear = jstack.stdlib()['clear']
+	assert(clear)
+	assert(clear.chunk == "stdlib")
+	assert(clear.content.type == "builtin")
+	assert(clear.help)
+	assert(clear.content.value)
+
+	local stack = {1, 2, 3}
+	assert(clear.content.value(nil, {}, stack))
+	assert(#stack == 0)
+end
+
+do
+	local f = jstack.stdlib()['get']
+	assert(f)
+	assert(f.chunk == "stdlib")
+	assert(f.content.type == "builtin")
+	assert(f.help)
+	assert(f.content.value)
+
+	-- TODO: actual behaviour
+end
+
+do
+	local f = jstack.stdlib()['is']
+	assert(f)
+	assert(f.chunk == "stdlib")
+	assert(f.content.type == "builtin")
+	assert(f.help)
+	assert(f.content.value)
+
+	-- TODO: actual behaviour
+end
+
+do
+	local f = jstack.stdlib()['to']
+	assert(f)
+	assert(f.chunk == "stdlib")
+	assert(f.content.type == "builtin")
+	assert(f.help)
+	assert(f.content.value)
+
+	-- TODO: actual behaviour
+end
+
+do
+	local f = jstack.stdlib()['assert']
+	assert(f)
+	assert(f.chunk == "stdlib")
+	assert(f.content.type == "builtin")
+	assert(f.help)
+	assert(f.content.value)
+
+	-- TODO: actual behaviour
+end
+
+do
+	local f = jstack.stdlib()['help']
+	assert(f)
+	assert(f.chunk == "stdlib")
+	assert(f.content.type == "builtin")
+	assert(f.help)
+	assert(f.content.value)
+
+	-- TODO: actual behaviour
+end
+
+do
+	local f = jstack.stdlib()['print']
+	assert(f)
+	assert(f.chunk == "stdlib")
+	assert(f.content.type == "builtin")
+	assert(f.help)
+	assert(f.content.value)
+
+	-- TODO: actual behaviour
+end
+
+do
+	local f = jstack.stdlib()['let']
+	assert(f)
+	assert(f.chunk == "stdlib")
+	assert(f.content.type == "builtin")
+	assert(f.help)
+	assert(f.content.value)
+
+	-- TODO: actual behaviour
+end
+
+do
+	local f = jstack.stdlib()['drop']
+	assert(f)
+	assert(f.chunk == "stdlib")
+	assert(f.content.type == "builtin")
+	assert(f.help)
+	assert(f.content.value)
+
+	-- TODO: actual behaviour
+end
+
+do
+	local f = jstack.stdlib()['swap']
+	assert(f)
+	assert(f.chunk == "stdlib")
+	assert(f.content.type == "builtin")
+	assert(f.help)
+	assert(f.content.value)
+
+	-- TODO: actual behaviour
+end
+
+do
+	local f = jstack.stdlib()['equal']
+	assert(f)
+	assert(f.chunk == "stdlib")
+	assert(f.content.type == "builtin")
+	assert(f.help)
+	assert(f.content.value)
+
+	-- TODO: actual behaviour
+end
+
+do
+	local f = jstack.stdlib()['copy']
+	assert(f)
+	assert(f.chunk == "stdlib")
+	assert(f.content.type == "builtin")
+	assert(f.help)
+	assert(f.content.value)
+
+	-- TODO: actual behaviour
+end
+
+do
+	local f = jstack.stdlib()['dupe']
+	assert(f)
+	assert(f.chunk == "stdlib")
+	assert(f.content.type == "builtin")
+	assert(f.help)
+	assert(f.content.value)
+
+	-- TODO: actual behaviour
+end
+
+do
+	local f = jstack.stdlib()['type']
+	assert(f)
+	assert(f.chunk == "stdlib")
+	assert(f.content.type == "builtin")
+	assert(f.help)
+	assert(f.content.value)
+
+	-- TODO: actual behaviour
+end
+
+do
+	local f = jstack.stdlib()['describe']
+	assert(f)
+	assert(f.chunk == "stdlib")
+	assert(f.content.type == "builtin")
+	assert(f.help)
+	assert(f.content.value)
+
+	-- TODO: actual behaviour
+end
+
+do
+	local f = jstack.stdlib()['import']
+	assert(f)
+	assert(f.chunk == "stdlib")
+	assert(f.content.type == "builtin")
+	assert(f.help)
+	assert(f.content.value)
+
+	-- TODO: actual behaviour
+end
+
+-- TODO: Other functions as we add them.
