263a4af1bc1ab45e2c944103a29b7871b7bca7d0
James Milne <s4b3r6@gmail.com>
2024-11-29 19:03:41 +1100

    More tests

--- tests/guess_filepath.lua
+++ tests/guess_filepath.lua
@@ -5,3 +5,6 @@ assert(jstack.guess_filepath)
 assert(type(jstack.guess_filepath) == "function")
 assert(jstack.guess_filepath("./examples/hello1") == "./examples/hello1.stk")
 assert(jstack.guess_filepath("./examples/scope") == "./examples/scope.stk")
+
+assert(jstack.guess_filepath("string") == "string")
+assert(jstack.guess_filepath("ffi") == "ffi")
