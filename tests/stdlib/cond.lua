60a79f790c3180c4a824a0c130919c5665808402
James Milne <s4b3r6@gmail.com>
2024-12-13 15:32:14 +1100

    Tests for stdlib cond

--- tests/stdlib/cond.lua
+++ tests/stdlib/cond.lua
@@ -12,6 +12,98 @@ do
 	assert(f.content.type == "builtin")
 	assert(f.help)
 	assert(f.content.value)
+end
 
-	-- TODO: actual behaviour
+do
+	local stack = {}
+	local prog = [==[
+	cond: {
+		{false} {"Nope."}
+		{true} {Here}
+	}
+	]==]
+	assert(jstack.eval(jstack.parse(prog), {jstack.stdlib()}, stack))
+	assert(#stack == 1)
+	assert(stack[1].content.type == "symbol")
+	assert(stack[1].content.value == "Here")
+end
+
+do
+	local stack = {}
+	local prog = [==[
+	cond: {
+		{true} {"Nope."}
+		{false} {Here}
+	}
+	]==]
+	assert(jstack.eval(jstack.parse(prog), {jstack.stdlib()}, stack))
+	assert(#stack == 1)
+	assert(stack[1].content.type == "string")
+	assert(stack[1].content.value == "Nope.")
+end
+
+do
+	local stack = {}
+	local prog = [==[
+	cond: {
+		{false} {"Nope."}
+		{false} {"Nope."}
+		{false} {"Nope."}
+		{false} {"Nope."}
+		{false} {"Nope."}
+		{false} {"Nope."}
+		{false} {"Nope."}
+		{false} {"Nope."}
+		{false} {"Nope."}
+		{false} {"Nope."}
+		{false} {"Nope."}
+		{false} {"Nope."}
+		{false} {"Nope."}
+		{true} {Here}
+	}
+	]==]
+	assert(jstack.eval(jstack.parse(prog), {jstack.stdlib()}, stack))
+	assert(#stack == 1)
+	assert(stack[1].content.type == "symbol")
+	assert(stack[1].content.value == "Here")
+end
+
+do
+	local stack = {}
+	local prog = [==[
+	let: a i21
+	cond: {
+		{equal? a$ i22} {"Nope."}
+		{equal? a$ i21} {Here}
+		{equal? a$ i23} {"Nope."}
+	}
+	]==]
+	assert(jstack.eval(jstack.parse(prog), {jstack.stdlib()}, stack))
+	assert(#stack == 1)
+	assert(stack[1].content.type == "symbol")
+	assert(stack[1].content.value == "Here")
+end
+
+do
+	local stack = {}
+	local prog = [==[
+	let: a i21
+	cond: {
+		{
+			"Nope."
+		}
+		{equal? a$ i22}
+		
+		{
+			Here
+		}
+		{equal? a$ i21}
+
+		{equal? a$ i23} {"Nope."}
+	}
+	]==]
+	assert(jstack.eval(jstack.parse(prog), {jstack.stdlib()}, stack))
+	assert(#stack == 1)
+	assert(stack[1].content.type == "symbol")
+	assert(stack[1].content.value == "Here")
 end
