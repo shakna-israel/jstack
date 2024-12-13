a5a9e8490bcc8398f507656d81fc982cf29d8900
James Milne <s4b3r6@gmail.com>
2024-12-13 15:42:30 +1100

    Test stdlib describe

--- tests/stdlib/describe.lua
+++ tests/stdlib/describe.lua
@@ -12,6 +12,35 @@ do
 	assert(f.content.type == "builtin")
 	assert(f.help)
 	assert(f.content.value)
+end
 
-	-- TODO: actual behaviour
+do
+	-- Leaves the expression on the stack:
+	local stack = {}
+	local prog = [==[
+	let: thing {
+		add: add:
+	}
+	describe: "Calls add twice." thing$
+	]==]
+	assert(jstack.eval(jstack.parse(prog), {jstack.stdlib()}, stack))
+	assert(#stack == 1)
+	assert(stack[1].content.type == "expression")
+end
+
+do
+	-- Actually installs help info:
+	local stack = {}
+	local prog = [==[
+	let: thing {
+		add: add:
+	}
+	describe: "Calls add twice." thing$
+	help!
+	]==]
+	assert(jstack.eval(jstack.parse(prog), {jstack.stdlib()}, stack))
+	assert(#stack == 1)
+	assert(stack[1].content.type == "string")
+	-- Further info is added, so we just check the start is correct:
+	assert(stack[1].content.value:sub(1, #"Calls add twice.") == "Calls add twice.")
 end
