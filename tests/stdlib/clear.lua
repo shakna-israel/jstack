6c129d58f2215e1aff276acd5cf5fd925cf4b298
James Milne <s4b3r6@gmail.com>
2024-12-13 15:26:36 +1100

    Expanded stdlib clear tests

--- tests/stdlib/clear.lua
+++ tests/stdlib/clear.lua
@@ -14,6 +14,38 @@ do
 	assert(clear.content.value)
 
 	local stack = {1, 2, 3}
+	assert(#stack > 0)
+	assert(clear.content.value(nil, {}, stack))
+	assert(#stack == 0)
+end
+
+do
+	local clear = jstack.stdlib()['clear']
+	assert(clear)
+	assert(clear.chunk == "stdlib")
+	assert(clear.content.type == "builtin")
+	assert(clear.help)
+	assert(clear.content.value)
+
+	local stack = {}
+	assert(clear.content.value(nil, {}, stack))
+	assert(#stack == 0)
+end
+
+do
+	local clear = jstack.stdlib()['clear']
+	assert(clear)
+	assert(clear.chunk == "stdlib")
+	assert(clear.content.type == "builtin")
+	assert(clear.help)
+	assert(clear.content.value)
+
+	local stack = {}
+	for i=1, 1000 do
+		stack[#stack + 1] = jstack.make_symbol(nil, true)
+	end
+	assert(#stack > 0)
+
 	assert(clear.content.value(nil, {}, stack))
 	assert(#stack == 0)
 end
