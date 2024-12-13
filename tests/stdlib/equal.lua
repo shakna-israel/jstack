f5ea4af9b8480036853f2379848d755c19d15a0f
James Milne <s4b3r6@gmail.com>
2024-12-13 16:00:29 +1100

    Partial test implementation of stdlib equal. (Expression comparison unimplemented)

--- tests/stdlib/equal.lua
+++ tests/stdlib/equal.lua
@@ -12,6 +12,55 @@ do
 	assert(f.content.type == "builtin")
 	assert(f.help)
 	assert(f.content.value)
-
-	-- TODO: actual behaviour
 end
+
+do
+	-- Empty stack turns into two nils:
+	local stack = {}
+	assert(jstack.eval(jstack.parse("equal:"), {jstack.stdlib()}, stack))
+	assert(#stack == 1)
+	assert(stack[1].content.type == "symbol")
+	assert(stack[1].content.value == "true")
+end
+
+do
+	local stack = {}
+	assert(jstack.eval(jstack.parse("equal: a \"a\""), {jstack.stdlib()}, stack))
+	assert(#stack == 1)
+	assert(stack[1].content.type == "symbol")
+	assert(stack[1].content.value == "false")
+end
+
+do
+	local stack = {}
+	assert(jstack.eval(jstack.parse("equal: a a"), {jstack.stdlib()}, stack))
+	assert(#stack == 1)
+	assert(stack[1].content.type == "symbol")
+	assert(stack[1].content.value == "true")
+end
+
+do
+	local stack = {}
+	assert(jstack.eval(jstack.parse("equal: i10 i10"), {jstack.stdlib()}, stack))
+	assert(#stack == 1)
+	assert(stack[1].content.type == "symbol")
+	assert(stack[1].content.value == "true")
+end
+
+do
+	local stack = {}
+	assert(jstack.eval(jstack.parse("equal: i10 f10"), {jstack.stdlib()}, stack))
+	assert(#stack == 1)
+	assert(stack[1].content.type == "symbol")
+	assert(stack[1].content.value == "false")
+end
+
+do
+	local stack = {}
+	assert(jstack.eval(jstack.parse("equal: f10 f10"), {jstack.stdlib()}, stack))
+	assert(#stack == 1)
+	assert(stack[1].content.type == "symbol")
+	assert(stack[1].content.value == "true")
+end
+
+-- TODO: Expression comparison...
