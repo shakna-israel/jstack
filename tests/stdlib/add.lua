48b8b779d1fce883c361f9a2c2e8e3518100f2c9
James Milne <s4b3r6@gmail.com>
2024-12-13 13:54:12 +1100

    Implemented + and add stdlib tests

--- tests/stdlib/add.lua
+++ tests/stdlib/add.lua
@@ -12,6 +12,36 @@ do
 	assert(f.content.type == "builtin")
 	assert(f.help)
 	assert(f.content.value)
+end
 
-	-- TODO: actual behaviour
+do
+	local stack = {}
+	assert(jstack.eval(jstack.parse("add: a b"), {jstack.stdlib()}, stack))
+	assert(#stack == 1)
+	assert(stack[1].content.type == "error")
+	assert(stack[1].content.value == "Type")
+end
+
+do
+	local stack = {}
+	assert(jstack.eval(jstack.parse("add: i10 f10.1"), {jstack.stdlib()}, stack))
+	assert(#stack == 1)
+	assert(stack[1].content.type == "error")
+	assert(stack[1].content.value == "Type")
+end
+
+do
+	local stack = {}
+	assert(jstack.eval(jstack.parse("add: i10 i-3"), {jstack.stdlib()}, stack))
+	assert(#stack == 1)
+	assert(stack[1].content.type == "integer")
+	assert(stack[1].content.value == 7)
+end
+
+do
+	local stack = {}
+	assert(jstack.eval(jstack.parse("add: f10 f-3.1"), {jstack.stdlib()}, stack))
+	assert(#stack == 1)
+	assert(stack[1].content.type == "float")
+	assert(stack[1].content.value == 6.9)
 end
