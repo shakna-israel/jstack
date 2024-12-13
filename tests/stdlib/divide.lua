bc38be5dbd560a448b9d040f2c66d52804ead782
James Milne <s4b3r6@gmail.com>
2024-12-13 15:47:24 +1100

    Implemented stdlib divide and /

--- tests/stdlib/divide.lua
+++ tests/stdlib/divide.lua
@@ -12,6 +12,52 @@ do
 	assert(f.content.type == "builtin")
 	assert(f.help)
 	assert(f.content.value)
+end
 
-	-- TODO: actual behaviour
+do
+	local stack = {}
+	assert(jstack.eval(jstack.parse("divide: a b"), {jstack.stdlib()}, stack))
+	assert(#stack == 1)
+	assert(stack[1].content.type == "error")
+	assert(stack[1].content.value == "Type")
+end
+
+do
+	local stack = {}
+	assert(jstack.eval(jstack.parse("divide: i10 f21"), {jstack.stdlib()}, stack))
+	assert(#stack == 1)
+	assert(stack[1].content.type == "error")
+	assert(stack[1].content.value == "Type")
+end
+
+do
+	local stack = {}
+	assert(jstack.eval(jstack.parse("divide: i10 i2"), {jstack.stdlib()}, stack))
+	assert(#stack == 1)
+	assert(stack[1].content.type == "integer")
+	assert(stack[1].content.value == 5)
+end
+
+do
+	local stack = {}
+	assert(jstack.eval(jstack.parse("divide: i3 i2"), {jstack.stdlib()}, stack))
+	assert(#stack == 1)
+	assert(stack[1].content.type == "integer")
+	assert(stack[1].content.value == 1)
+end
+
+do
+	local stack = {}
+	assert(jstack.eval(jstack.parse("divide: f10 f2"), {jstack.stdlib()}, stack))
+	assert(#stack == 1)
+	assert(stack[1].content.type == "float")
+	assert(stack[1].content.value == 5.0)
+end
+
+do
+	local stack = {}
+	assert(jstack.eval(jstack.parse("divide: f3 f2"), {jstack.stdlib()}, stack))
+	assert(#stack == 1)
+	assert(stack[1].content.type == "float")
+	assert(stack[1].content.value == 1.5)
 end
--- tests/stdlib/divsym.lua
+++ tests/stdlib/divsym.lua
@@ -15,3 +15,51 @@ do
 
 	-- TODO: actual behaviour
 end
+
+do
+	local stack = {}
+	assert(jstack.eval(jstack.parse("/: a b"), {jstack.stdlib()}, stack))
+	assert(#stack == 1)
+	assert(stack[1].content.type == "error")
+	assert(stack[1].content.value == "Type")
+end
+
+do
+	local stack = {}
+	assert(jstack.eval(jstack.parse("/: i10 f21"), {jstack.stdlib()}, stack))
+	assert(#stack == 1)
+	assert(stack[1].content.type == "error")
+	assert(stack[1].content.value == "Type")
+end
+
+do
+	local stack = {}
+	assert(jstack.eval(jstack.parse("/: i10 i2"), {jstack.stdlib()}, stack))
+	assert(#stack == 1)
+	assert(stack[1].content.type == "integer")
+	assert(stack[1].content.value == 5)
+end
+
+do
+	local stack = {}
+	assert(jstack.eval(jstack.parse("/: i3 i2"), {jstack.stdlib()}, stack))
+	assert(#stack == 1)
+	assert(stack[1].content.type == "integer")
+	assert(stack[1].content.value == 1)
+end
+
+do
+	local stack = {}
+	assert(jstack.eval(jstack.parse("/: f10 f2"), {jstack.stdlib()}, stack))
+	assert(#stack == 1)
+	assert(stack[1].content.type == "float")
+	assert(stack[1].content.value == 5.0)
+end
+
+do
+	local stack = {}
+	assert(jstack.eval(jstack.parse("/: f3 f2"), {jstack.stdlib()}, stack))
+	assert(#stack == 1)
+	assert(stack[1].content.type == "float")
+	assert(stack[1].content.value == 1.5)
+end
