3d357087bd93d7b7bc4bcfe6850e2c6aed202e5e
James Milne <s4b3r6@gmail.com>
2024-12-13 15:18:07 +1100

    Tests for stdlib cos^-1 and acos

--- tests/stdlib/acos.lua
+++ tests/stdlib/acos.lua
@@ -12,6 +12,52 @@ do
 	assert(f.content.type == "builtin")
 	assert(f.help)
 	assert(f.content.value)
+end
 
-	-- TODO: actual behaviour
+do
+	local stack = {}
+	assert(jstack.eval(jstack.parse("acos: a"), {jstack.stdlib()}, stack))
+	assert(#stack == 1)
+	assert(stack[1].content.type == "error")
+	assert(stack[1].content.value == "Type")
+end
+
+do
+	local stack = {}
+	assert(jstack.eval(jstack.parse("acos: i10"), {jstack.stdlib()}, stack))
+	assert(#stack == 1)
+	assert(stack[1].content.type == "integer")
+	assert(stack[1].content.value ~= stack[1].content.value)
+end
+
+do
+	local stack = {}
+	assert(jstack.eval(jstack.parse("acos: f10"), {jstack.stdlib()}, stack))
+	assert(#stack == 1)
+	assert(stack[1].content.type == "float")
+	assert(stack[1].content.value ~= stack[1].content.value)
+end
+
+do
+	local stack = {}
+	assert(jstack.eval(jstack.parse("acos: i1"), {jstack.stdlib()}, stack))
+	assert(#stack == 1)
+	assert(stack[1].content.type == "integer")
+	assert(stack[1].content.value == 0)
+end
+
+do
+	local stack = {}
+	assert(jstack.eval(jstack.parse("acos: i0"), {jstack.stdlib()}, stack))
+	assert(#stack == 1)
+	assert(stack[1].content.type == "integer")
+	assert(stack[1].content.value == 1)
+end
+
+do
+	local stack = {}
+	assert(jstack.eval(jstack.parse("acos: f0.5"), {jstack.stdlib()}, stack))
+	assert(#stack == 1)
+	assert(stack[1].content.type == "float")
+	assert(stack[1].content.value == math.acos(0.5))
 end
--- tests/stdlib/acossym.lua
+++ tests/stdlib/acossym.lua
@@ -15,3 +15,51 @@ do
 
 	-- TODO: actual behaviour
 end
+
+do
+	local stack = {}
+	assert(jstack.eval(jstack.parse("cos^-1: a"), {jstack.stdlib()}, stack))
+	assert(#stack == 1)
+	assert(stack[1].content.type == "error")
+	assert(stack[1].content.value == "Type")
+end
+
+do
+	local stack = {}
+	assert(jstack.eval(jstack.parse("cos^-1: i10"), {jstack.stdlib()}, stack))
+	assert(#stack == 1)
+	assert(stack[1].content.type == "integer")
+	assert(stack[1].content.value ~= stack[1].content.value)
+end
+
+do
+	local stack = {}
+	assert(jstack.eval(jstack.parse("cos^-1: f10"), {jstack.stdlib()}, stack))
+	assert(#stack == 1)
+	assert(stack[1].content.type == "float")
+	assert(stack[1].content.value ~= stack[1].content.value)
+end
+
+do
+	local stack = {}
+	assert(jstack.eval(jstack.parse("cos^-1: i1"), {jstack.stdlib()}, stack))
+	assert(#stack == 1)
+	assert(stack[1].content.type == "integer")
+	assert(stack[1].content.value == 0)
+end
+
+do
+	local stack = {}
+	assert(jstack.eval(jstack.parse("cos^-1: i0"), {jstack.stdlib()}, stack))
+	assert(#stack == 1)
+	assert(stack[1].content.type == "integer")
+	assert(stack[1].content.value == 1)
+end
+
+do
+	local stack = {}
+	assert(jstack.eval(jstack.parse("cos^-1: f0.5"), {jstack.stdlib()}, stack))
+	assert(#stack == 1)
+	assert(stack[1].content.type == "float")
+	assert(stack[1].content.value == math.acos(0.5))
+end
