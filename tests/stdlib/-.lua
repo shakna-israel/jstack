02def8a81fe7d0b45d7c3151403d00df1f013416
James Milne <s4b3r6@gmail.com>
2024-12-13 13:58:04 +1100

    - and take stdlib tests

--- tests/stdlib/-.lua
+++ tests/stdlib/-.lua
@@ -12,6 +12,36 @@ do
 	assert(f.content.type == "builtin")
 	assert(f.help)
 	assert(f.content.value)
+end
 
-	-- TODO: actual behaviour
+do
+	local stack = {}
+	assert(jstack.eval(jstack.parse("-: a b"), {jstack.stdlib()}, stack))
+	assert(#stack == 1)
+	assert(stack[1].content.type == "error")
+	assert(stack[1].content.value == "Type")
+end
+
+do
+	local stack = {}
+	assert(jstack.eval(jstack.parse("-: i10 f10.1"), {jstack.stdlib()}, stack))
+	assert(#stack == 1)
+	assert(stack[1].content.type == "error")
+	assert(stack[1].content.value == "Type")
+end
+
+do
+	local stack = {}
+	assert(jstack.eval(jstack.parse("-: i10 i-3"), {jstack.stdlib()}, stack))
+	assert(#stack == 1)
+	assert(stack[1].content.type == "integer")
+	assert(stack[1].content.value == 13)
+end
+
+do
+	local stack = {}
+	assert(jstack.eval(jstack.parse("-: f10 f-3.1"), {jstack.stdlib()}, stack))
+	assert(#stack == 1)
+	assert(stack[1].content.type == "float")
+	assert(stack[1].content.value == 13.1)
 end
