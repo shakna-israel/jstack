83cc7cfe9892046a90aea446231b757fcd892ffc
James Milne <s4b3r6@gmail.com>
2024-12-13 16:29:35 +1100

    Tests for stdlib get

--- tests/stdlib/get.lua
+++ tests/stdlib/get.lua
@@ -12,6 +12,41 @@ do
 	assert(f.content.type == "builtin")
 	assert(f.help)
 	assert(f.content.value)
+end
 
-	-- TODO: actual behaviour
+do
+	local stack = {}
+	assert(jstack.eval(jstack.parse("get: a"), {jstack.stdlib()}, stack))
+	assert(#stack == 1)
+	assert(stack[1].content.type == "symbol")
+	assert(stack[1].content.value == "nil")
+end
+
+do
+	local stack = {}
+	local stdlib = jstack.stdlib()
+	assert(jstack.eval(jstack.parse("get: get"), {stdlib}, stack))
+	assert(#stack == 1)
+	assert(stack[1].content.type == "builtin")
+	assert(stack[1].content.value == stdlib['get'].content.value)
+end
+
+do
+	local stack = {}
+	local stdlib = jstack.stdlib()
+	local prog = [=[
+	let: a i21
+	{
+		let: a i17
+		get: a
+	}
+	!
+	get: a
+	]=]
+	assert(jstack.eval(jstack.parse(prog), {stdlib}, stack))
+	assert(#stack == 2)
+	assert(stack[1].content.type == "integer")
+	assert(stack[1].content.value == 17)
+	assert(stack[2].content.type == "integer")
+	assert(stack[2].content.value == 21)
 end
